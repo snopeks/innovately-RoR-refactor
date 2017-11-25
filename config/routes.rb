86dcc35c429377a0b571f1a839533ddda1ac64c2
snopeks <creativesnopeks@gmail.com>
2017-11-24 23:40:22 -0800

    include SessionsHelper and create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'sessions/new'
+
+  root to: "users#index"
+  get "/", to: "users#index"
+  get "/new", to: "users#new", as: "new_user"
+  post "/users", to: "users#create"
+  get "/users/:id", to: "users#show", as: "user"
+  get "/users/:user_id/blogs", to: "blogs#index", as: "user_blogs"
+  get "/login", to: "sessions#new"
+  get "/logout", to: "sessions#destroy"
+  post "/sessions", to: "sessions#create"
 end
